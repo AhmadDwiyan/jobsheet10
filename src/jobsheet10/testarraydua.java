d50f1021bf23b7c6fd89ff535fc4f6885ff26b70
GL62 <GL62@MSI>
2018-08-22 05:34:47 -0800

    Tugas Jobsheet8

---
+++ src/jobsheet10/testarraydua.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet10;
+
+/**
+ *
+ * @author ASUS
+ */
+public class testarraydua {
+public static void main(String args []){
+int nis [][] = {{210651},{210651},{210652}};
+String nama[][] = {{"Ade"},{"Galih "},{"Baihaqi"}};
+
+System.out.println("Identitas Siswa Angkatan 24");
+
+
+for (int i = 0; i< 1; i++){
+for (int j = 0; j < 1; j++){
+System.out.println(nama[0][j]+":"+nis[0][j]);
+testarraydua siswa = new testarraydua();
+System.out.println(siswa);
+}
+}
+}
+
+
+
+
+}
+
+
+
+  
